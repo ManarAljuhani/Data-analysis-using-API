886728f3b7f95967b755d755b95292dfcea99b0c
ManarAljuhani <mabsj52@gmail.com>
2020-09-04 20:19:39 +0300

    second commit :)

--- database_service.py
+++ database_service.py
@@ -1,48 +1,62 @@
 from flask import Flask, jsonify
 from flask import request
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON
+import numpy as np
+import pandas as pd
+import pymysql
+import psycopg2
 
-#initialize flask app 
+connection = psycopg2.connect(user = "postgres",
+                                  password = "123456",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "db9")
+
+#initialize flask app
 app =Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 #default route to 127.0.0.1:3000
-@app.route('/')
-def home():
-    return"HELLO WORLD" 
 
-@app.route('/get_data_count/<label_name>,<count1>', methods=['GET'])
+@app.route('/get_data_count/, methods=['GET'])
 
 def get_data_count(label_name, count):
-   
+
    try:
       label_name = str(request.args.get('label_name'))
-      
       count = int(request.args.get('count'))
-      
-      result ="your chosen label name is".format(label_name)
-
-      return jsonify(result)
+      if label_name == 'positive':
+          result=cursor.excute(" SELECT  CLASSIFECATION_NUM FROM  data_input1 where CLASSIFECATION_NUM = 1 LIMIT = '%S'" , count)
+      elif label_name == 'negative':
+          result=cursor.excute(" SELECT  CLASSIFECATION_NUM FROM  data_input1 where CLASSIFECATION_NUM = 0 LIMIT = '%s'", count)
+      else:
+          result=cursor.excute(" SELECT  CLASSIFECATION_NUM FROM  data_input1 LIMIT = '%s'", count)
+      result= cursor.fetchall()
+      return jsonify(len(result))
    except:
-      return "Error in using method get_sum!"
-@app.route('/get_data/<int> , <int>', methods=['GET'])
+      return "Error in using method get_data_count!"
 
-def get_data(int,int):
-   
-   try:
-      count = int(request.args.get('count'))
-      
-      sort_order= int(request.args.get('sort_order'))
-       
+   @app.route('/get_data/, methods=['GET'])
 
-      return jsonify(result)
-   except:
-      return "Error in using method get_sum!"
+   def get_data(count1, sort_order):
+      try:
+         count1 = int(request.args.get('count1'))
+         sort_order = int(request.args.get('sort_order'))
+         if sort_order == 'ASC':
+            result = cursor.excute("SELECT TEXT FROM  data_input LIMIT = '%d' ORDER BY timestamp ASC", count1)
+         else:
+            result = cursor.excute("SELECT TEXT FROM  data_input LIMIT = '%d' ORDER BY timestamp DESC", count1)
+         result = cursor.fetchall()
+         return jsonify(result)
 
+      except:
+         return "Error in using method get_data!"
 
-#stat the API by port 3000
+   connection.commit()
+   connection.close()
 if __name__ == "__main__":
   app.run(host= '0.0.0.0', port=3000)
+
