c8d1370e7421d91ee90829aa05f8c02bc6e03c8d
ManarAljuhani <63353557+ManarAljuhani@users.noreply.github.com>
2020-09-04 20:54:12 +0300

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,13 +1,11 @@
 import requests
 from flask import jsonify
 
-
-payload = {'var1': 1, 'var2': 3}
-result =requests.get("http://0.0.0.0:3000/get_sum", payload, headers= {"Content-Type": "application/json"})
+payload = {'label_name': "positive", 'count': 15}
+payload1 = {'sort_order':'ASC', 'count1': 20}
+result =requests.get("http://0.0.0.0:3000/get_data", payload, headers= {"Content-Type": "application/json"})
+result =requests.get("http://0.0.0.0:3000/get_data_count", payload1, headers= {"Content-Type": "application/json"})
 print(result.json())
-
-
-
 
 
 
